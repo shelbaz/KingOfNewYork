dabd7db00cb2e8668f59590e407b54315d03379b
Shawn Elbaz <elbaz.shawn@gmail.com>
2018-10-10 22:25:44 -0400

    Dice class and header implementation

---
+++ King of New York/King of New York/Dice.cpp
@@ -0,0 +1,88 @@
+//
+// Created by Shawn Elbaz on 2018-10-10.
+//
+
+#include "Dice.h"
+#include <random>
+#include <ctime>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+Dice::Dice(int playerVal) {
+    srand((unsigned)time(0));
+
+    playerNumber = playerVal;
+    numberOfRolls = 3;
+
+    map<DiceOptions, int> DiceValues;
+    vector< map < DiceOptions, int> > historyOfRolls;
+
+    vector< map < DiceOptions, int> > ::iterator it;
+    map<DiceOptions, int>::iterator mapIt;
+
+}
+
+Dice::DiceOptions Dice::randomDiceOption() {
+    return static_cast<DiceOptions>(rand() % DiceOptions::NumOfOptions);
+
+}
+
+void Dice::resetDiceRolls() {
+    this->numberOfRolls = 3;
+
+}
+
+void Dice::rollDice(int amtOfDice) {
+
+    if(this->numberOfRolls == 0)
+    {
+        return;
+    }
+
+DiceOptions value;
+    for(int i=0; i<amtOfDice; i++)
+    {
+        value = randomDiceOption();
+        this->DiceValues[value] ++;
+    }
+    storeDiceResult(DiceValues);
+    resetDiceValuesMap();
+    this->numberOfRolls--;
+
+}
+
+void Dice::storeDiceResult(map<DiceOptions, int> tempDiceValues) {
+    historyOfRolls.push_back(tempDiceValues);
+
+}
+
+int Dice::getPlayerNumber() {
+    return playerNumber;
+}
+
+void Dice::resetDiceValuesMap() {
+    DiceValues[DiceOptions::Heal] = 0;
+    DiceValues[DiceOptions::Attack] = 0;
+    DiceValues[DiceOptions::Celebrity] = 0;
+    DiceValues[DiceOptions::Destruction] = 0;
+    DiceValues[DiceOptions::Energy] = 0;
+    DiceValues[DiceOptions::Ouch] = 0;
+}
+
+
+void Dice::diceHistoricalValues()
+{
+    cout << "Dice container values for player "<< this->playerNumber  << endl;
+    cout << "-----------------------------------------------------------------" << endl;
+    for (it = historyOfRolls.begin(); it != historyOfRolls.end(); ++it) {
+        for (map<DiceOptions, int>::iterator mapIt(it->begin()); mapIt != it->end(); ++mapIt) {
+
+            // output here
+            std::cout << mapIt->first << ", " << mapIt->second << std::endl;
+        }
+    }
+
+
+}
---
+++ King of New York/King of New York/Dice.h
@@ -0,0 +1,40 @@
+//
+// Created by Shawn Elbaz on 2018-10-10.
+//
+
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+class Dice {
+
+    enum DiceOptions{Energy, Attack, Destruction, Heal, Celebrity, Ouch, NumOfOptions};
+
+public:
+    Dice(int playerVal);
+    void rollDice(int amtOfDice);
+    void storeDiceResult(map<DiceOptions, int> tempDiceValues);
+    int getPlayerNumber();
+    void resetDiceValuesMap();
+    void resetDiceRolls();
+    void diceHistoricalValues();
+    DiceOptions randomDiceOption();
+
+
+
+private:
+    int playerNumber;
+    int amtOfDice;
+    int numberOfRolls;
+    map<DiceOptions, int> DiceValues = {{Energy, 0}, {Attack, 0}, {Destruction, 0}, {Heal, 0} , {Celebrity, 0} , {Ouch, 0}};
+    map<DiceOptions, int>::iterator mapIt;
+    vector< map < DiceOptions, int> > historyOfRolls;
+    vector< map < DiceOptions, int> > ::iterator it;
+
+
+
+
+};
