34441f1fb3a06125d7b22c0b38c25d0efcd3f86e
Shawn Elbaz <elbaz.shawn@gmail.com>
2018-11-04 23:47:14 -0500

    Add game driver

--- King of New York/Drivers/GameDriver.cpp
+++ King of New York/Drivers/GameDriver.cpp
@@ -2,3 +2,49 @@
 // Created by Shawn Elbaz on 2018-11-04.
 //
 
+#include "Player.h"
+#include "DeckOfBuildinigUnitTiles.h"
+#include "DeckOfCards.h"
+#include "MonsterCards.h"
+#include "GameTokens.h"
+#include "Game.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    Game game;
+    game.init_all_decks();
+    int numbOfPlayers = game.init_players();
+    vector<Player*> players = game.getGamePlayers();
+
+//    for (int i=0; i< numbOfPlayers; i++){
+//        cout << *players[i];
+//    }
+    game.determinePlayerOrder();
+    players = game.getGamePlayersAfter();
+    for (int i=0; i< players.size(); i++){
+        cout << *players[i];
+    }
+
+
+    cout << "NEW PLAYER ORDER ----------------------------------" << endl;
+
+    for (int i=0; i< players.size(); i++){
+        cout << *players[i];
+    }
+
+    game.removePlayer(players[3]);
+    players = game.getGamePlayersAfter();
+
+    cout << "removed player ----------------------------------" << endl;
+
+
+    for (int i=0; i< players.size(); i++){
+        cout << *players[i];
+    }
+
+
+    return 0;
+}
